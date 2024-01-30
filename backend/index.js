c78246e9f98ecf9e5f00a2bf4389c7c3093dbc89
navneetnavs <navneetkumar2004@icloud.com>
2024-01-30 15:05:53 +0530

    application completed

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,25 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
-const app = express();
 
 dotenv.config();
+
 app.use(express.json());
-mongoose
-.connect(process.env.MONGO_URL, {
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-    useCreateIndex : true,
-})
-.then(()=>{
-    console.log("database connected")
-})
-.catch((err => console.log(err)));
+
+mongoose 
+ .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,   })   
+ .then(() => console.log("MongoDB connected!"))
+ .catch(err => console.log(err));
 
 app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
 
-app.listen(8800, ()=> {
-    console.log("Backend is running")
-})
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
